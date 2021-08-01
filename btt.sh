e5b26a19bf6dc09a4199d98f4e7d7eb9fcb331ca
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2021-08-01 12:57:37 +0530

    Create btt.sh

---
+++ btt.sh
@@ -0,0 +1,8 @@
+# BTT
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+tar xf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2/
+./violetminer -c --algorithm chukwa --pool rx.unmineable.com:3333 --username BTT:TKEUa3SvCJdN3XA4oJnhQNALHBqGJjSgLV.vasava1+50000
