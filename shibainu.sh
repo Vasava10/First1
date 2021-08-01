40d73a6c550ff23253f7359fa7631d6d3308ce4e
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2021-08-01 13:01:49 +0530

    Update and rename shibainu (1).sh to shibainu.sh

--- shibainu.sh
+++ shibainu.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
-tar xf violetminer-linux-v0.2.2.tar.gz
+tar -xf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2/
 ./violetminer -c --algorithm chukwa --pool rx.unmineable.com:3333 --username SHIB:0x17A72Bb901fEA40fbDfC8006B51FF6C29b3cF4a9.vasava1+50000
