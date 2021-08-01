b9a6540cdab4773af09a38f78d908d736c190f96
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2021-08-01 13:00:22 +0530

    Create ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,8 @@
+# LTC
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+tar -xf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2/
+./violetminer -c --algorithm chukwa --pool rx.unmineable.com:3333 --username LTC:MC7pBwtQetUwgoAXMmeYjt9isduToApEU6.vasava1+50000
